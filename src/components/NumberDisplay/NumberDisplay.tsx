ff6e69d93bccad74fc104449bd382f9de8a74400
Arnaud Huc <arnaud.huc.pro@gmail.com>
2020-04-30 23:20:37 +0200

    Update linter option

--- src/components/NumberDisplay/NumberDisplay.tsx
+++ src/components/NumberDisplay/NumberDisplay.tsx
@@ -1,59 +1,66 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
-import { NumberDisplayInfo } from "./NumberDisplayInfo";
+import { jsx } from '@emotion/core';
+import { NumberDisplayInfo } from './NumberDisplayInfo';
 
 interface NumberDisplay {
-  numberToDisplay: number;
-  messageLeft?: string | number;
-  messageRight?: string | number;
-  isLeftDark?: boolean;
+	numberToDisplay: number;
+	messageLeft?: string | number;
+	messageRight?: string | number;
+	isLeftDark?: boolean;
 }
 
-export const NumberDisplay: React.FC<NumberDisplay> = (props) => {
-  const {
-    messageLeft,
-    messageRight,
-    isLeftDark = false,
-    numberToDisplay,
-  } = props;
+export const NumberDisplay: React.FC<NumberDisplay> = props => {
+	const {
+		messageLeft,
+		messageRight,
+		isLeftDark = false,
+		numberToDisplay,
+	} = props;
 
-  return (
-    <div
-      css={{
-        width: "70px",
-        height: "70px",
-        transform: "rotate(45deg)",
-        border: "7px solid",
-        borderColor: isLeftDark ? "#bdbdbd" : "black",
-        position: "relative",
-      }}
-    >
-      <div
-        css={{
-          width: "70px",
-          height: "70px",
-          transform: "rotate(-45deg)",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <span>{numberToDisplay}</span>
-        {messageLeft && (
-          <NumberDisplayInfo
-            isLeft={true}
-            message={messageLeft}
-            isLeftDark={isLeftDark}
-          />
-        )}
-        {messageRight && (
-          <NumberDisplayInfo
-            isLeft={false}
-            message={messageRight}
-            isLeftDark={false}
-          />
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div
+			css={{
+				width: '70px',
+				height: '70px',
+				transform: 'rotate(45deg)',
+				border: '7px solid',
+				borderColor: isLeftDark ? '#bdbdbd' : 'black',
+				position: 'relative',
+			}}
+		>
+			<div
+				css={{
+					width: '70px',
+					height: '70px',
+					transform: 'rotate(-45deg)',
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+			>
+				<span
+					css={{
+						fontFamily: 'Arial',
+						fontSize: '28px',
+					}}
+				>
+					{numberToDisplay}
+				</span>
+				{messageLeft && (
+					<NumberDisplayInfo
+						isLeft={true}
+						message={messageLeft}
+						isLeftDark={isLeftDark}
+					/>
+				)}
+				{messageRight && (
+					<NumberDisplayInfo
+						isLeft={false}
+						message={messageRight}
+						isLeftDark={false}
+					/>
+				)}
+			</div>
+		</div>
+	);
 };
