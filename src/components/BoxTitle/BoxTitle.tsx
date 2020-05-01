4d982455c32c15d313685df48e596d813c9b8f1c
Arnaud Huc <arnaud.huc.pro@gmail.com>
2020-05-02 01:03:08 +0200

    Add Marker and Alternative BoxTitle

--- src/components/BoxTitle/BoxTitle.tsx
+++ src/components/BoxTitle/BoxTitle.tsx
@@ -3,22 +3,23 @@ import { jsx } from '@emotion/core';
 
 interface IBoxTitle {
 	title: string;
+	isLight?: boolean;
 }
 
 export const BoxTitle: React.FC<IBoxTitle> = (props) => {
-	const { title } = props;
+	const { title, isLight = false } = props;
 	return (
 		<div
 			css={{
-				background: 'black',
+				background: isLight ? '#8d8d8d' : 'black',
 				color: 'white',
 				height: '30px',
-				textAlign: 'center',
 				display: 'flex',
 				alignItems: 'center',
-				justifyContent: 'center',
+				justifyContent: isLight ? 'left' : 'center',
 				textTransform: 'uppercase',
 				fontSize: '20px',
+				paddingLeft: isLight ? '10px' : '0',
 			}}
 		>
 			{title}
