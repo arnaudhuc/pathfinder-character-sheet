2c9cd9d2112bc9528673cfd992482ad639976fda
Arnaud Huc <arnaud.huc.pro@gmail.com>
2020-05-03 23:59:05 +0200

    Begin Tile

---
+++ src/components/Tiles/Tiles.tsx
@@ -0,0 +1,6 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+
+export const Tiles = () => {
+	return <div>Here comme the tiles</div>;
+};
---
+++ src/components/Tiles/stories/Tiles.stories.tsx
@@ -0,0 +1,6 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import { storiesOf } from '@storybook/react';
+import { Tiles } from '../Tiles';
+
+storiesOf('Tiles', module).add('Default', () => <Tiles />);
