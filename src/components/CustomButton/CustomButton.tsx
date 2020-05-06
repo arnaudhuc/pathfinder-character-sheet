50e248fefb645e5c170b06d476dfb62b0d40c1ce
Arnaud Huc <arnaud.huc.pro@gmail.com>
2020-05-06 15:30:52 +0200

    add material ui's theme for storybook

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -1,15 +1,6 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import { common } from '../../style/common';
 import Button from '@material-ui/core/Button';
-import Box from '@material-ui/core/Box';
-import { useMemo } from 'react';
-import {
-	createMuiTheme,
-	useMediaQuery,
-	ThemeProvider,
-} from '@material-ui/core';
-import { blue, green } from '@material-ui/core/colors';
 
 interface IButton {
 	value: string;
@@ -18,26 +9,11 @@ interface IButton {
 
 export const CustomButton: React.FC<IButton> = (props) => {
 	const { value, handleClick } = props;
-	const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-	const theme = useMemo(
-		() =>
-			createMuiTheme({
-				palette: {
-					type: prefersDarkMode ? 'dark' : 'light',
-					primary: blue,
-					secondary: green,
-				},
-			}),
-		[prefersDarkMode],
-	);
-
 	return (
 		<div>
-			<ThemeProvider theme={theme}>
-				<Button onClick={handleClick} variant="contained" color="primary">
-					{value}
-				</Button>
-			</ThemeProvider>
+			<Button onClick={handleClick} variant="contained" color="primary">
+				{value}
+			</Button>
 		</div>
 	);
 };
