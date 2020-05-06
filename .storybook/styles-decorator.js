0f07f187f55cd3303a35da74a48c696cc3a5a1d6
Arnaud Huc <arnaud.huc.pro@gmail.com>
2020-05-06 15:37:28 +0200

    use one theme

--- .storybook/styles-decorator.js
+++ .storybook/styles-decorator.js
@@ -4,13 +4,13 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { StylesProvider } from '@material-ui/styles';
 
-import LightTheme from '../src/themes/light-theme';
+import Theme from '../src/themes/theme';
 
 const StylesDecorator = (storyFn) => (
 	<StylesProvider injectFirst>
 		<CssBaseline />
-		<StyledThemeProvider theme={LightTheme}>
-			<MuiThemeProvider theme={LightTheme}>{storyFn()}</MuiThemeProvider>
+		<StyledThemeProvider theme={Theme}>
+			<MuiThemeProvider theme={Theme}>{storyFn()}</MuiThemeProvider>
 		</StyledThemeProvider>
 	</StylesProvider>
 );
