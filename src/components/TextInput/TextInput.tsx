502e5115808ac1c70798478ee8b8f35107b147b4
Arnaud Huc <ahuc@oui.sncf>
2020-05-29 15:15:20 +0200

    Add Form validation
    Update dependency @types/lodash to v4.14.154
    
    Update dependency @material-ui/core to v4.10.1
    
    Update dependency @typescript-eslint/parser to v3.1.0
    
    Update dependency @typescript-eslint/eslint-plugin to v3.1.0
    
    Update dependency @testing-library/react to v10.0.5
    
    Update dependency @testing-library/user-event to v10.4.1
    
    Update dependency @types/node to v12.12.43

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -6,14 +6,23 @@ import React from 'react';
 interface ITextInput {
 	label?: string;
 	ref?: any;
+	isError?: boolean;
+	errorMessage?: string;
+	handleOnChange?: () => void;
 }
 
 export const TextInput: React.FC<ITextInput> = React.forwardRef(
 	(props, ref) => {
-		const { label } = props;
+		const { label, isError = false, errorMessage = '', handleOnChange } = props;
 		return (
 			<div css={{ display: 'flex' }}>
-				<TextField label={label} inputRef={ref} />
+				<TextField
+					label={label}
+					inputRef={ref}
+					error={isError}
+					helperText={errorMessage}
+					onChange={handleOnChange}
+				/>
 			</div>
 		);
 	},
